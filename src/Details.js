517b92aaa05804ab611d214732e2168739a45ffc
eGathergood <ewan_good@hotmail.co.uk>
2020-10-25 18:29:10 +0000

    Added simple routing usnig reach/router

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Details = () => {
-  return <h1>eijowwfe</h1>;
+const Details = (props) => {
+  return (
+    <h1>winwdinwdindwi</h1>
+    //This is a nice way to debug props/state etc
+    // <pre>
+    //   <code>{JSON.stringify(props, null, 4)}</code>
+    // </pre>
+  );
 };
 
 export default Details;
